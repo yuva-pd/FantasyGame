4f9755f99e8d8eef440b7c2f1ddea59e4923703c
yuva-pd <81077158+yuva-pd@users.noreply.github.com>
2024-01-08 19:49:52 +0530

    Create styles.js

---
+++ Desktop/demo/src/ResultScreen/styles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container:{
+          backgroundColor: '#2C358A',
+          width: '120%',
+          height: 50,
+        },
+  heading:{color: 'white',fontSize:20,top:11,left:95},
+  image:{height: 25, width: 25},
+  name:{fontSize: 20, marginRight: 5},
+  cubconatiner:{
+          padding: 10,
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          backgroundColor: '#D5E9FF',
+          marginBottom: 5,
+          width: '95%',
+          marginLeft: 9,
+        }
+})
