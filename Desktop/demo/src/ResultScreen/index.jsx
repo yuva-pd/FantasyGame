69bd5d049af46d262500a5f5e1885103540693d4
yuva-pd <81077158+yuva-pd@users.noreply.github.com>
2024-01-08 19:49:56 +0530

    Update index.jsx

--- Desktop/demo/src/ResultScreen/index.jsx
+++ Desktop/demo/src/ResultScreen/index.jsx
@@ -7,6 +7,7 @@ import {
   Image,
 } from 'react-native';
 import React from 'react';
+import {styles} from './styles';
 
 const index = ({route}) => {
   const {selectedPlayers} = route.params;
@@ -16,24 +17,15 @@ const index = ({route}) => {
   const renderItem = ({item}) => {
     return (
       <View
-        style={{
-          padding: 10,
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          backgroundColor: '#D5E9FF',
-          marginBottom: 5,
-          width: '95%',
-          marginLeft: 9,
-        }}>
+        style={styles.subcontainer}>
         <View>
-          <Text style={{fontSize: 20, marginRight: 5}}>{item.name}</Text>
+          <Text style={styles.name}>{item.name}</Text>
           <Text style={{marginTop: 10}}>{item.role}</Text>
         </View>
         <View>
           <Image
             source={{uri: item.team_logo}}
-            style={{height: 25, width: 25}}
+            style={styles.image}
           />
           <Text style={{marginTop: 13}}>{item.team_name}</Text>
         </View>
@@ -43,14 +35,8 @@ const index = ({route}) => {
   return (
     <View style={{flex: 1}}>
       <View
-        style={{
-          backgroundColor: '#2C358A',
-          width: '120%',
-          height: 50,
-          // padding: 25,
-
-        }}>
-        <Text style={{color: 'white',fontSize:20,top:11,left:95}}>Selected Players List</Text>
+        style={styles.container}>
+        <Text style={styles.heading}>Selected Players List</Text>
       </View>
       <ScrollView>
         <FlatList
